05c4014a0a91eeb05c8459f82e29b066a5a3121a
Nicolas Operiol Gerbal <nicolas.operiol-gerbal@etu.univ-orleans.fr>
2015-05-09 14:22:49 +0000

    Ajout de methodes de Guimera

---
+++ Code/TER/include/GuimeraAmaral.h
@@ -0,0 +1,23 @@
+#ifndef GUIMERAAMARAL_H
+#define GUIMERAAMARAL_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <malloc.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+class GuimeraAmaral {
+ public:
+
+
+
+#endif // GUIMERAAMARAL_H
