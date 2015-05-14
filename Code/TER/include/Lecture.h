0e4809c8d071f5502bf7719cda152d85c6b434d6
Clement Desmazeaud <clement.desmazeaud@etu.univ-orleans.fr>
2015-05-14 14:10:55 +0000

    Oubli du Lecture.h

---
+++ Code/TER/include/Lecture.h
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+#include <vector>
+
+#include "GrapheOriente.h"
+
+#ifndef LECTURE_H
+#define LECTURE_H
+
+GrapheOriente* lecture(std::string nom);
+
+#endif
