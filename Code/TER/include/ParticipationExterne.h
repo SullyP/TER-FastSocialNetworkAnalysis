969731cc257ac65a824bd1535851bc3ded3537af
Clement Desmazeaud <clement.desmazeaud@etu.univ-orleans.fr>
2015-05-14 18:15:21 +0000

    Participation externe

---
+++ Code/TER/include/ParticipationExterne.h
@@ -0,0 +1,9 @@
+#include "Communaute.h"
+#include "GrapheNonOriente.h"
+
+#ifndef EXTERNE_H
+#define EXTERNE_H
+
+std::vector<int> participationExterne(Communaute communaute, int noeud);
+
+#endif
---
+++ Code/TER/src/ParticipationExterne.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <math.h>
+#include "ParticipationExterne.h"
+
+using namespace std;
+
+vector<int> participationExterne(Communaute communaute, int num_com){
+    const GrapheNonOriente* graphe = communaute.getGraphe();
+    vector<int> listCommunaute = communaute.getNoeudCommunaute();
+    vector<double> moyenne;
+    vector<int> moyenneNoeud;
+    vector<bool> visiteCommunaute;
+    vector<int> participation;
+    int cpt = 0;
+    int nbCommunaute = 0;
+
+    for (int i = 0; i < listCommunaute.size(); i++){
+        if (listCommunaute[i] > nbCommunaute){
+            nbCommunaute = listCommunaute[i];
+        }
+    }
+
+    for(int i = 0; i <= nbCommunaute; i++){
+        visiteCommunaute.push_back(false);
+        moyenneNoeud.push_back(0);
+    }
+
+    for(int i = 0; i < 3; i++){
+        participation.push_back(0);
+    }
+
+    for(int noeud = 0; noeud < listCommunaute.size(); noeud++){
+
+        if (listCommunaute[noeud] == num_com){
+            moyenne.push_back(0);
+            vector<Arc> voisins = graphe->getArcsSortants(noeud);
+            for(int i = 0; i < voisins.size(); i++){
+                if (listCommunaute[voisins[i].getNumeroSommet()] != listCommunaute[noeud]){
+
+                    if (!visiteCommunaute[listCommunaute[voisins[i].getNumeroSommet()]]){
+                        participation[0]++;
+                        visiteCommunaute[listCommunaute[voisins[i].getNumeroSommet()]] = true;
+                    }
+
+                    moyenneNoeud[listCommunaute[voisins[i].getNumeroSommet()]]++;
+                    participation[1]++;
+                }
+            }
+
+            int comVisite = 0;
+            int total = 0;
+            for (int i = 0; i < moyenneNoeud.size(); i++){
+                if (moyenneNoeud[i] != 0){
+                    comVisite++;
+                    total += moyenneNoeud[i];
+                }
+            }
+
+            moyenne[cpt] = total/comVisite;
+
+            for(int i = 0; i <= nbCommunaute; i++){
+                moyenneNoeud.push_back(0);
+            }
+            cpt++;
+
+        }
+    }
+
+    double hetero = 0;
+    double moyenneTotal = 0;
+
+    for (int i = 0; i < moyenne.size(); i++){
+        hetero += moyenne[i]*moyenne[i];
+        moyenneTotal++;
+    }
+
+    hetero /= moyenneTotal;
+
+    participation[2] = sqrt(hetero);
+
+    participation[1] /= cpt;
+
+    return participation;
+}
